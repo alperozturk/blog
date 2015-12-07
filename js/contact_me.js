767faa0a1b2a59cbcba231737f13c92b728a5dd8
alperozturk <alperozturk83@yahoo.com>
2015-12-07 02:26:05 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Thank you " + firstName + ", I will answer you e-mail ASAP!");
+                    $('#success > .alert-danger').append("<strong>Thank you " + firstName + ", I will answer your e-mail ASAP!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
